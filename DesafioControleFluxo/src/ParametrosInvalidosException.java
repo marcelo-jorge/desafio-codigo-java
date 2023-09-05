67509ba7a39c9b88cc35a10d3f6917414ad7aa91
marcelo-jorge <celodc@yahoo.com.br>
2023-09-05 03:15:50 -0300

    update: Mensagem de exceção

--- DesafioControleFluxo/src/ParametrosInvalidosException.java
+++ DesafioControleFluxo/src/ParametrosInvalidosException.java
@@ -1,3 +1,8 @@
 public class ParametrosInvalidosException extends Exception {
+
+    @Override
+    public String getMessage(){
+        return "O segundo parâmetro deve ser maior que o primeiro";
+    }
     
 }
