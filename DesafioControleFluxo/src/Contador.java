b04fe2dbedae70c40217d983fe9d3828d192e7f7
marcelo-jorge <celodc@yahoo.com.br>
2023-09-05 03:03:21 -0300

    update: Contador,  metodo contar

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -1,5 +1,28 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Contador {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("digite o primeiro numero inteiro: ");
+        int n1 = scanner.nextInt();
+
+        System.out.println("digite o segundo numero inteiro maior: ");
+        int n2 = scanner.nextInt();
+
+        
+    }
+
+    static void contar(int n1,int n2)throws ParametrosInvalidosException{
+
+        if(n1 > n2)
+            throw new ParametrosInvalidosException();
+
+        int contador = n2-n1;
+        for(int i=1 ; i<=contador ; i++){
+            System.out.println("imprimindo numero: " + i );
+        }
+
     }
 }
