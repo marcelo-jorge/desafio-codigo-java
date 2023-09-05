ec9f5d3eeaa3dd9cfddd699f720068b04b72dbef
marcelo-jorge <celodc@yahoo.com.br>
2023-09-05 03:21:08 -0300

    update: Comentarios no codigo

--- DesafioControleFluxo/src/Contador.java
+++ DesafioControleFluxo/src/Contador.java
@@ -3,17 +3,25 @@ import java.util.Scanner;
 
 public class Contador {
     public static void main(String[] args) throws Exception {
+        // Classe scanner utilizada para ler no terminal
         Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
+        // solicita ao usuario o primeiro numero inteiro
         System.out.println("digite o primeiro numero inteiro: ");
         int n1 = scanner.nextInt();
 
+        // solicita ao usuario o segundo numero inteiro
         System.out.println("digite o segundo numero inteiro maior: ");
         int n2 = scanner.nextInt();
 
+        // realizar uma tentativa pra o metodo contar
         try {
+
             contar(n1, n2);            
+
+        // caso o primeiro numeo seja maior que o segundo lançara uma exceção
         } catch (ParametrosInvalidosException pie) {
+            // busca a mensagem sobrecarregada na classe de exceções. 
             System.out.println(pie.getMessage());
         }
         
@@ -21,9 +29,11 @@ public class Contador {
 
     static void contar(int n1,int n2)throws ParametrosInvalidosException{
 
+        // executa uma verificação, em caso de verdadeiro lançara uma exceção
         if(n1 > n2)
             throw new ParametrosInvalidosException();
 
+        //caso a exceção não seja lançada executa o resto do codigo
         int contador = n2-n1;
         for(int i=1 ; i<=contador ; i++){
             System.out.println("imprimindo numero: " + i );
--- DesafioControleFluxo/src/ParametrosInvalidosException.java
+++ DesafioControleFluxo/src/ParametrosInvalidosException.java
@@ -1,5 +1,6 @@
 public class ParametrosInvalidosException extends Exception {
 
+    // sobrecarrega um metodo da classe exception que exibe uma mensagem quando o exeção é lançada
     @Override
     public String getMessage(){
         return "O segundo parâmetro deve ser maior que o primeiro";
