c1c3b58dc77ef6e603f01f5c894b79b8bb25bdc7
marcelo-jorge <celodc@yahoo.com.br>
2023-09-15 02:25:11 -0300

    udpate: finalizando teste da classe

--- collections-java-api-2023/src/list/ordenacao/OrdenacaoPessoas.java
+++ collections-java-api-2023/src/list/ordenacao/OrdenacaoPessoas.java
@@ -2,7 +2,6 @@ package list.ordenacao;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 
 public class OrdenacaoPessoas {
@@ -26,5 +25,23 @@ public class OrdenacaoPessoas {
         List<Pessoa> listaPorAltura = new ArrayList<>(listaDePessoas);
         Collections.sort(listaPorAltura, new ComparatorPorAltura());
         return listaPorAltura;
+    }
+
+    public static void main(String[] args) {
+        OrdenacaoPessoas ordenacaoPessoas = new OrdenacaoPessoas();
+
+        //adcionar Pessoas
+        ordenacaoPessoas.adcionarPessoa("nome 1", 25, 1.80);
+        ordenacaoPessoas.adcionarPessoa("nome 2", 26, 1.60);
+        ordenacaoPessoas.adcionarPessoa("nome 3", 30, 1.90);
+        ordenacaoPessoas.adcionarPessoa("nome 4", 28, 1.70);
+        ordenacaoPessoas.adcionarPessoa("nome 5", 18, 1.60);
+        System.out.println(ordenacaoPessoas.listaDePessoas);
+        
+        //Ordenar por Idade
+        System.out.println(ordenacaoPessoas.ordenarPorIdade());
+
+        //Ordenar por Altura
+        System.out.println(ordenacaoPessoas.ordenarPorAltura());
     }
 }
