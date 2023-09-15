c7f45e88a97b527f86a8786b0a02216dd6e9e78d
marcelo-jorge <celodc@yahoo.com.br>
2023-09-15 02:44:45 -0300

    update: metodos ordem ascendente e descendente

--- collections-java-api-2023/src/list/ordenacao/OrdenacaoNumeros.java
+++ collections-java-api-2023/src/list/ordenacao/OrdenacaoNumeros.java
@@ -1,5 +1,38 @@
 package list.ordenacao;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class OrdenacaoNumeros {
+    private List<Integer> listaDeInteiros;
+
+    public OrdenacaoNumeros() {
+        this.listaDeInteiros = new ArrayList<>();
+    }
+
+    //adcionar numero
+    public void adicionarNumero(Integer n){
+        listaDeInteiros.add(n);
+    }
     
+    //ordenar ascendente
+    public List<Integer> ordenarAscendente(){
+        List<Integer> listaAscendente = new ArrayList<>(listaDeInteiros);
+        Collections.sort(listaAscendente);
+        return listaAscendente;
+    }
+
+    //ordenar descendente
+    public List<Integer> ordenarDescendente(){
+        List<Integer> listaDescendente = new ArrayList<>(listaDeInteiros);
+        Collections.sort(listaDescendente);
+        return listaDescendente;
+    }
+
+       
+
+    //adcionar numero
+    //ordenar ascendente
+    //ordenar descendente
 }
