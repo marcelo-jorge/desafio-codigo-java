6d65a7fa7a09ec1517b5bd83c9cf0b0b250f079f
marcelo-jorge <celodc@yahoo.com.br>
2023-09-15 08:47:11 -0300

    update: finalizado o exercicio Palavras unicas

--- collections-java-api-2023/src/set/operacoes_basicas/ConjuntoPalavrasUnicas.java
+++ collections-java-api-2023/src/set/operacoes_basicas/ConjuntoPalavrasUnicas.java
@@ -19,16 +19,42 @@ public class ConjuntoPalavrasUnicas {
     //remover
     public void removerPalavra(String palavra){
         conjuntoPalavrasUnicas.remove(palavra);
+        System.out.println("Removendo: " + palavra);
     }
 
     //verificar
-    public boolean verificarPalavra(String palavra){
+    public boolean verificarPalavra(String palavra){        
         return conjuntoPalavrasUnicas.contains(palavra);
     }
 
     //exibir
     public void exibirPalavrasUnicas(){
         System.out.println(conjuntoPalavrasUnicas);
+    }
+
+    public static void main(String[] args) {
+        
+        ConjuntoPalavrasUnicas conjuntoPalavrasUnicas = new ConjuntoPalavrasUnicas();
+
+        //adcionar
+        conjuntoPalavrasUnicas.adicionarPalavra("Marcelo");
+        conjuntoPalavrasUnicas.adicionarPalavra("Jose");
+        conjuntoPalavrasUnicas.adicionarPalavra("Alfredo");
+        conjuntoPalavrasUnicas.adicionarPalavra("Gustavo");
+        conjuntoPalavrasUnicas.adicionarPalavra("Augusto");
+        conjuntoPalavrasUnicas.adicionarPalavra("Adriano");
+        conjuntoPalavrasUnicas.adicionarPalavra("Alfredo");
+        conjuntoPalavrasUnicas.exibirPalavrasUnicas();
+
+        //remover
+        conjuntoPalavrasUnicas.removerPalavra("Alfredo");
+        conjuntoPalavrasUnicas.exibirPalavrasUnicas();
+
+        //verificar
+        System.out.println("Marcelo esta no conjunto: " + conjuntoPalavrasUnicas.verificarPalavra("Marcelo"));        
+
+        //exibir
+        conjuntoPalavrasUnicas.exibirPalavrasUnicas();
     }
     
 }
