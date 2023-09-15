7dfe31919f3635da63009f2884bec324b60ebd34
marcelo-jorge <celodc@yahoo.com.br>
2023-09-15 02:50:09 -0300

    update: teste iniciais da classe

--- collections-java-api-2023/src/list/ordenacao/OrdenacaoNumeros.java
+++ collections-java-api-2023/src/list/ordenacao/OrdenacaoNumeros.java
@@ -30,9 +30,25 @@ public class OrdenacaoNumeros {
         return listaDescendente;
     }
 
-       
+    public static void main(String[] args) {
+        OrdenacaoNumeros ordenacaoNumeros = new OrdenacaoNumeros();
 
-    //adcionar numero
-    //ordenar ascendente
-    //ordenar descendente
+        //adcionar numero
+        ordenacaoNumeros.adicionarNumero(9);
+        ordenacaoNumeros.adicionarNumero(8);
+        ordenacaoNumeros.adicionarNumero(6);
+        ordenacaoNumeros.adicionarNumero(1);
+        ordenacaoNumeros.adicionarNumero(5);
+        ordenacaoNumeros.adicionarNumero(3);
+        ordenacaoNumeros.adicionarNumero(4);
+        ordenacaoNumeros.adicionarNumero(2);
+        ordenacaoNumeros.adicionarNumero(7);
+        System.out.println("Lista Original: " + ordenacaoNumeros.listaDeInteiros);
+        
+        //ordenar ascendente
+        System.out.println("Lista ordem crescente: " + ordenacaoNumeros.ordenarAscendente());
+        
+        //ordenar descendente
+        System.out.println("Lista ordem decrescente: " + ordenacaoNumeros.ordenarDescendente());        
+    }   
 }
