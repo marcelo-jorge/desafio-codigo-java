2d7581ed87e601664e8fc83d9f6edb99e28e8a4d
marcelo-jorge <celodc@yahoo.com.br>
2023-09-19 09:05:31 -0300

    create: classe Produto.java

--- collections-java-api-2023/src/set/ordenacao/Produto.java
+++ collections-java-api-2023/src/set/ordenacao/Produto.java
@@ -1,5 +1,58 @@
 package set.ordenacao;
 
+import java.util.Objects;
+
 public class Produto {
+    private long codigo;
+    private String nome;    
+    private double preco;
+    private int quantidade;
     
+    public Produto(long codigo, String nome, double preco, int quantidade) {
+        this.codigo = codigo;
+        this.nome = nome;
+        this.preco = preco;
+        this.quantidade = quantidade;
+    }
+
+    public long getCodigo() {
+        return codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCodigo());
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Produto other = (Produto) obj;
+        if (codigo != other.codigo)
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return codigo + "," + nome + "," + preco + "," + quantidade;
+    }
+
 }
