59e45c2aea81bed1a3c60d9170a5a5a2ebd8eabe
marcelo-jorge <celodc@yahoo.com.br>
2023-09-19 05:07:00 -0300

    update: Tarefa.java atributos classes

--- collections-java-api-2023/src/set/pesquisa/Tarefa.java
+++ collections-java-api-2023/src/set/pesquisa/Tarefa.java
@@ -1,5 +1,28 @@
 package set.pesquisa;
 
 public class Tarefa {
+    private String descricao;
+    private boolean concluido;
+    
+    public Tarefa(String descricao, boolean concluido) {
+        this.descricao = descricao;
+        this.concluido = concluido;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public boolean isConcluido() {
+        return concluido;
+    }
+
+    @Override
+    public String toString() {
+        return "Tarefa = " + descricao + ", concluido= " + concluido;
+    }
+
+    
+
     
 }
