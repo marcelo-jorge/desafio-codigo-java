9434099c3a6aa89afdeb90fcf5f9478c5942cacd
marcelo-jorge <celodc@yahoo.com.br>
2023-09-22 21:49:09 -0300

    update: EstoqueProduto finalizado classe

--- collections-java-api-2023/src/map/pesquisa/EstoqueProdutos.java
+++ collections-java-api-2023/src/map/pesquisa/EstoqueProdutos.java
@@ -31,4 +31,47 @@ public class EstoqueProdutos {
         return valorTotalEstoque;
     }
     
+    //obter produto mais caro
+    public Produto obterProdutoMaisCaro(){
+        Produto produtoMaisCaro = null;
+        double maiopreco = Double.MIN_VALUE;
+        for(Produto p: estoqueProdutosMap.values()){
+            if(p.getPreco() > maiopreco){
+                produtoMaisCaro = p;
+                maiopreco = p.getPreco();
+            }
+        }
+        return produtoMaisCaro;
+    }
+
+    //obter produto mais barato
+    public Produto obterProdutoMaisBarato(){
+        Produto produtoMaisBarato = null;
+        double menorPreco = Double.MAX_VALUE;
+
+        for (Produto p: estoqueProdutosMap.values()) {
+            if(p.getPreco() < menorPreco){
+                produtoMaisBarato = p;
+                menorPreco = p.getPreco();
+            }
+        }
+
+        return produtoMaisBarato;
+    }
+
+    //obter produto maior quantidade valor total no estoque
+    public Produto obterProdutoMaiorQuantidadeValorTotalNoEstoque(){
+        Produto produtoMQVE = null;
+        double maiorVTPE = 0d;
+        if(!estoqueProdutosMap.isEmpty()){
+            for(Map.Entry<Long,Produto> entry: estoqueProdutosMap.entrySet()){
+                double valorPEE = entry.getValue().getPreco() * entry.getValue().getQuantidade();
+                if(valorPEE > maiorVTPE){
+                    maiorVTPE = valorPEE;
+                    produtoMQVE = entry.getValue();
+                }
+            }
+        }
+        return produtoMQVE;
+    }
 }
