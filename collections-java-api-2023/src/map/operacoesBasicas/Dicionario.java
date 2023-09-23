b21d1b5ccbc453e8431b937d06ecef7e213657a6
marcelo-jorge <celodc@yahoo.com.br>
2023-09-22 21:02:40 -0300

    update: Dicionario iniciando testes

--- collections-java-api-2023/src/map/operacoesBasicas/Dicionario.java
+++ collections-java-api-2023/src/map/operacoesBasicas/Dicionario.java
@@ -43,4 +43,15 @@ public class Dicionario {
         }
     }
 
+    public static void main(String[] args) {
+        Dicionario dicionario = new Dicionario();
+
+        //adcionar palavras
+        dicionario.adicionarPalavra("java", "Linguagem de programação orientada a objetos.");
+        dicionario.adicionarPalavra("typescript", "Superset da linguagem JavaScript que adiciona tipagem estática.");
+        dicionario.adicionarPalavra("kotlin", "Linguagem moderna de programação para a JVM.");
+
+        dicionario.exibirPalavras();
+    }
+
 }
