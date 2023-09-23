d90d79da4f2bf002f3125fdb6c1dc4b404ae31d1
marcelo-jorge <celodc@yahoo.com.br>
2023-09-22 22:15:38 -0300

    update: EstoqueProduto finalizado testes

--- collections-java-api-2023/src/map/pesquisa/EstoqueProdutos.java
+++ collections-java-api-2023/src/map/pesquisa/EstoqueProdutos.java
@@ -11,7 +11,7 @@ public class EstoqueProdutos {
     }
 
     //adcionar
-    public void adcionarProduto(long cod, String nome, int quantidade, double preco){
+    public void adicionarProduto(long cod, String nome, int quantidade, double preco){
         estoqueProdutosMap.put(cod, new Produto(nome, preco, quantidade));
     }
 
@@ -74,4 +74,36 @@ public class EstoqueProdutos {
         }
         return produtoMQVE;
     }
+
+    public static void main(String[] args) {
+        EstoqueProdutos estoque = new EstoqueProdutos();
+
+        //exibe estoque vazio
+        estoque.exibirProdutos();
+        
+        // Adiciona produtos ao estoque
+        estoque.adicionarProduto(1L, "Notebook", 1, 1500.0);
+        estoque.adicionarProduto(2L, "Mouse", 5, 25.0);
+        estoque.adicionarProduto(3L, "Monitor", 10, 400.0);
+        estoque.adicionarProduto(4L, "Teclado", 2, 40.0);
+
+        //exibe estoque vazio
+        estoque.exibirProdutos();
+
+        //calcula valor total do estoque
+        System.out.println("Valor Total: " + estoque.calcularValorTotalEstoque());        
+
+        //obtem o produto mais caro
+        Produto produtoMaisCaro = estoque.obterProdutoMaisCaro();
+        System.out.println("mais caro " + produtoMaisCaro);
+
+        //obtem o produto mais barato
+        Produto produtoMaisBarato = estoque.obterProdutoMaisBarato();
+        System.out.println("mais barato " + produtoMaisBarato);
+
+        //obtem e exibe prodt=uto com maior quantidade valor em estoque
+        Produto produtoMQVT = estoque.obterProdutoMaiorQuantidadeValorTotalNoEstoque();
+        System.out.println("Maior quantidade em valor no estoque " + produtoMQVT);
+
+        }
 }
